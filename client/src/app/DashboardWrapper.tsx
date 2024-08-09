0d8ceb6a27b17812a3f9f092146bd0cf9040f891
D_Zucho <danny.nico.dev@outlook.com>
2024-08-09 16:54:01 -0700

    start navbar skeleton

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Navbar from '@/app/components/Navbar'
 
 type Props = {}
 
@@ -9,6 +10,7 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24`}
       >
+        <Navbar />
         {children}
       </main>
     </div>
